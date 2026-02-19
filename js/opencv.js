33e3b5b8007bfa60c043a79b31aa96bcb43c56db
cloud-blokpax <jamespoto@gmail.com>
2026-02-19 10:54:04 -0500

    Delete js directory

--- js/opencv.js
+++
@@ -1,9 +0,0 @@
-// OpenCV - DISABLED (Not needed for current scanning flow)
-
-function loadOpenCV() {
-    console.log('⏭️ OpenCV disabled - not required for scanning');
-    ready.cv = false;
-    return Promise.resolve();
-}
-
-console.log('✅ OpenCV module loaded (disabled)');
