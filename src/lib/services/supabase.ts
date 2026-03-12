20168f9017b8458654eb6911aa3ec3e7ababa9ed
cloud-blokpax <jamespoto@gmail.com>
2026-03-12 16:03:15 -0400

    Delete src directory

--- src/lib/services/supabase.ts
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr';
-import { env } from '$env/dynamic/public';
-import type { Database } from '$lib/types/database';
-
-export const supabase = createBrowserClient<Database>(
-	env.PUBLIC_SUPABASE_URL ?? '',
-	env.PUBLIC_SUPABASE_ANON_KEY ?? ''
-);
